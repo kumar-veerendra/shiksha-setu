48fc2fb68df437e62618c0660621134958f74a69
Veerendra Kumar <veer8298979904@gmail.com>
2025-08-30 22:38:09 +0530

    backend video call feature

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,17 +4,36 @@ import {createServer} from "node:http";
 import { Server } from "socket.io";
 
 import mongoose from "mongoose";
-
 import cors from "cors";
 
-const app = express();
+import userRoutes from "./routes/users.routes.js";
 
-app.get("/", (req, res) => {
-    return res.send("Hello World!");
-});
+import { connectToSocket } from "./controllers/socketManager.js";
+
+
+
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000));
+
+app.use(cors());
+app.use(express.json({ limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true}))
+
+app.use("/api/v1/users", userRoutes);
+
+// app.get("/", (req, res) => {
+//     return res.send("Hello World!");
+// });
 
 const start = async () => {
-    app.listen(8000, () => {
+    const connectionDB = await mongoose.connect("mongodb+srv://shikshasetu100:shikshasetu@shikshasetu.muqfua4.mongodb.net/");
+    console.log(`MONGO Connected DB Host : ${connectionDB.connection.host}`)
+
+    server.listen(app.get("port"), () => {
         console.log("Listening on PORT 8000...");
     });
 }
