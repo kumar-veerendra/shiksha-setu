7d842f0f4b505b8607abf2bba353e15286589e9d
Veerendra Kumar <veer8298979904@gmail.com>
2026-03-21 18:55:03 +0530

    update backend URL

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -7,10 +7,18 @@
 
 // export default server;
 
-let IS_PROD = false;
+// let IS_PROD = false;
+
+// const server = IS_PROD
+//   ? "https://apnacollegebackend.onrender.com"
+//   : "http://localhost:8000";  // ✅ your laptop IP
+
+// export default server;
+
+let IS_PROD = true;
 
 const server = IS_PROD
-  ? "https://apnacollegebackend.onrender.com"
-  : "http://localhost:8000";  // ✅ your laptop IP
+  ? "https://shiksha-setu-production.up.railway.app/"
+  : "http://localhost:8000";
 
 export default server;
